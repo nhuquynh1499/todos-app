2e1e467edee066f5387ed9f63f75c21120a61e7a
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-07-10 09:49:19 +0700

    Fix UI for app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import AddIcon from '../images/add.svg'
 
 class Footer extends Component {
   render() {
     const { onClick } = this.props;
     return (
       <div className="Footer" onClick={onClick}>
-        <img className="" src="https://cdn.glitch.com/e41838d3-90fc-48cf-a257-5d476d13e374%2Fadd.svg?v=1592046561907" />
+        <img src={AddIcon}/>
       </div>
     )
   }
