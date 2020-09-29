af0647cd7a900cf26ba9b26c2b6b6ca10d21a740
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-09-29 20:50:26 +0700

    Changed from class to functional for Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import "./style.css";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        {"dailist".split("").map((item) => {
-          return <span>{item}</span>;
-        })}
-      </div>
-    );
-  }
+export default function Header(props) {
+  return (
+    <div className="Header">
+      {"dailist".split("").map((item) => {
+        return <span>{item}</span>;
+      })}
+    </div>
+  );
 }
-
-export default Header;
